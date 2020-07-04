787f9e541784b35fa8be64db3bbf4b0841792a92
Kevin Jolley <serrowpls@gmail.com>
2020-07-03 19:46:30 -0500

    object destructuring and conversion hours

--- scripts.js
+++ scripts.js
@@ -4,9 +4,47 @@ import Raiders from './users.js';
 import RaidersObj from './users.js';
 
 $(document).ready(() => {
-  // FormRaid();
-  NewRaid();
+  GenRaid();
 });
+
+function GenRaid() {
+  // Access object, remove individual items, pass to constructor
+  // I want user/rank/image from each, just the string
+
+  const raiders = Object.entries(RaidersObj['RaidersObj']);
+
+  raiders.forEach(([key, value]) => {
+    console.log(key);
+    console.log(value);
+
+    $('#raiders').append(`
+      <div class="raider-card-wrapper">
+        <div class="raider-block">
+          <p class="raider-title"> ${value.user} </p>
+        </div>
+        <img src=${value.classimg} class="raid-img" alt="uwu" />
+      </div>
+    `);
+  });
+}
+
+let Serrow = new RaiderCard('Serrow', './assets/png/druid.png', 'Officer');
+
+function RaiderCard(name, img, rank) {
+  this.name = name;
+  this.img = img;
+  this.rank = rank;
+}
+
+function NewRoster(raider) {
+  $('#raiders').append(`
+    <div class="raider">
+      <h1> ${raider.name} </h1>
+      <h1> ${raider.rank} </h1>
+      <img src=${raider.img} />
+    </div>
+  `);
+}
 
 function FormRaid() {
   return Raiders['Raiders'].forEach((item) => {
@@ -19,5 +57,13 @@ function NewRaid() {
 
   return raids.forEach((raider) => {
     $('#item-container').append(`<p> ${raider} </p>`);
+  });
+}
+
+function Roster() {
+  let raids = Object.keys(RaidersObj['RaidersObj']);
+
+  return raids.forEach((raider) => {
+    $('#raiders').append(`<p class="raider-card"> ${raider} </p>`);
   });
 }
--- users.js
+++ users.js
@@ -7,26 +7,37 @@ const RaidersObj = {
     spec: 'Balance',
     small: 'https://render-us.worldofwarcraft.com/character/malganis/173/178962861-avatar.jpg',
     large: 'https://render-us.worldofwarcraft.com/character/malganis/173/178962861-main.jpg',
+    classimg: './assets/png/druid.png',
   },
   Santo: {
     user: 'Santo',
     large: 'https://render-us.worldofwarcraft.com/character/malganis/224/173798880-main.jpg',
     class: 'Warlock',
+    classimg: './assets/png/mage.png',
   },
   Ethren: {
     user: 'Ethren',
     large: 'https://render-us.worldofwarcraft.com/character/malganis/195/180125635-main.jpg',
     class: 'Demon Hunter',
+    classimg: './assets/png/druid.png',
   },
   Ryanlockwood: {
     user: 'Ryanlockwood',
     large: 'https://render-us.worldofwarcraft.com/character/malganis/233/153922281-main.jpg',
     class: 'Paladin',
+    classimg: './assets/png/mage.png',
   },
   Neon: {
     user: 'Neon',
     large: 'https://render-us.worldofwarcraft.com/character/malganis/223/180819679-main.jpg',
     class: 'Death Knight',
+    classimg: './assets/png/druid.png',
+  },
+  Nysti: {
+    user: 'Nysti',
+    large: 'https://render-us.worldofwarcraft.com/character/malganis/223/180819679-main.jpg',
+    class: 'Death Knight',
+    classimg: './assets/png/mage.png',
   },
 };
 
